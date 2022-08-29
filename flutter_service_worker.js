b743a230cffb873b8d99096f1b034f611e5cf079
amoserlu <87151749+amoserlu@users.noreply.github.com>
2022-08-29 18:09:16 +0200

    arreglo fallo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f0bfaa7ded4f361cdfada1866453b67c",
-"/": "f0bfaa7ded4f361cdfada1866453b67c",
+"index.html": "4f9c0e80da253433f60918479e473fc7",
+"/": "4f9c0e80da253433f60918479e473fc7",
 "main.dart.js": "58f158d550933b24079e3512134bc9ab",
 "manifest.json": "14533758d5adebb52c270151b0335f00",
 "version.json": "1ac57eb0214a8f5216f1da7dd479b33f"
